8e8f9f6c754b5b7e8067587cf214800064e197ad
Ray <shreyanair2104@gmail.com>
2026-03-08 16:30:59 +0530

    create the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,16 +9,16 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-      <img src = {assets.logo} alt = "" className="logo" />
-      <ul className = "navbar-menu">
-        <li onClick={()=>setMenu("home")}  className={menu==="home" ? "active" : ""}>home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu" ? "active" : ""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app" ? "active" : ""}>mobile-app</li>
-        <li onClick={()=>setMenu("contact us")} className={menu==="contact us" ? "active" : ""}>contact us</li>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
+        <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
+        <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+        <li onClick={() => setMenu("contact us")} className={menu === "contact us" ? "active" : ""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className = "navbar-search-icon">
+        <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
