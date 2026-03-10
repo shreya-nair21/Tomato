bfc3c36bcc8b29b678bc90bc119361843f6c98cf
Ray <shreyanair2104@gmail.com>
2026-03-10 15:06:56 +0530

    add images + content for food display

--- frontend/src/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../context/StoreContext'
+import FoodItem from '../components/FoodItem/FoodItem';
 
 const FoodDisplay = ({ category }) => {
 
@@ -10,7 +11,9 @@ const FoodDisplay = ({ category }) => {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return
+          if (category === "All" || category === item.category) {
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+          }
         })}
       </div>
     </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../FoodDisplay/FoodDisplay'
 
 const Home = () => {
 
@@ -11,6 +12,7 @@ const Home = () => {
 
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category}/>
     </div>
   )
 }
