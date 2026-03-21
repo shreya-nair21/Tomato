59eaabc040bb89a34e0e918e7eea8a6932beed7d
Ray <shreyanair2104@gmail.com>
2026-03-21 21:33:24 +0530

    display dot on cart icon

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
 
   const [menu, setMenu] = useState("home");
+
+  const { getTotalCartAmount } = useContext(StoreContext)
 
   return (
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
-      
+
       <ul className="navbar-menu">
-        <Link 
-          to='/' 
-          onClick={() => setMenu("home")} 
+        <Link
+          to='/'
+          onClick={() => setMenu("home")}
           className={menu === "home" ? "active" : ""}
         >
           home
         </Link>
-        
+
         {/* Anchor tags for scroll-to-section remain as <a> */}
         <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
         <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
@@ -30,9 +33,9 @@ const Navbar = ({setShowLogin}) => {
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-          <div className="dot"></div>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   )
