29cfbc4fdedfc75695179c216d9e5dd1d6a1b4db
Ray <shreyanair2104@gmail.com>
2026-03-29 01:04:14 +0530

    Implement food removal and listing functionality in food routes

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -4,8 +4,8 @@ import fs from 'fs'
 // add food item
 
 const addFood = async (req, res) => {
-    let image_filename = `${req.file.filename}`;
-
+    
+let image_filename = `${req.file.filename}`;
     const food = new foodModel({
         name: req.body.name,
         description: req.body.description,
@@ -24,4 +24,29 @@ const addFood = async (req, res) => {
     }
 }
 
-export { addFood }
+// all food list
+const listFood = async(req, res) => {
+    try{
+        const foods = await foodModel.find({})
+        res.json({success:true, data:foods})
+    }catch(error){
+        console.log(error);
+        res.json({success:false, message:error})
+    }
+}
+
+// remove food item
+const removeFood = async(req, res) => {
+    try{
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}` , () => {})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Food Removed"})
+    }catch(error){
+        console.log(error);
+        res.json({success: false, message: "Error"})
+        
+    }
+}
+export { addFood, listFood, removeFood }
