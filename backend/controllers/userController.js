0f6bc8ac9983d253868f5be2876c672ceb62565d
Ray <shreyanair2104@gmail.com>
2026-04-10 16:43:52 +0530

    create api for registering user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,7 @@ const createToken =  (id) => {
   return jwt.sign({id}, process.env.JWT_SECRET)
 }
 
-//register user
+//api to register user
 const registerUser = async (req, res) => {
     const {name, password, email} = req.body;
     try{
@@ -30,7 +30,7 @@ const registerUser = async (req, res) => {
           return res.json({success: false, message: "Please enter a strong password"})
         }
 
-        //hashing user password
+        //hashing user password 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
