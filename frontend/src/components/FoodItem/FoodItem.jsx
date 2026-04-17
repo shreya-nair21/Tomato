81f01b7f1e037309a5fab88e6cb698774a70fd53
Ray <shreyanair2104@gmail.com>
2026-04-17 19:18:16 +0530

    fix

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,26 +5,24 @@ import { StoreContext } from '../../context/StoreContext';
 
 
 const FoodItem = ({ id, name, price, description, image }) => {
-    const {cartItems, addToCart, removeFromCart} = useContext(StoreContext)
+    const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext)
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
-                <img className='food-item-image' src={image} alt={name} />
-                
-                {/* 1. If itemCount is 0, show ONLY the white plus icon */}
+                <img className='food-item-image' src={url + "/images/" + image} alt={name} />
+
                 {!cartItems[id]
                     ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
                     : <div className='food-item-counter'>
                         <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
                         <p>{cartItems[id]}</p>
                         <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
-                      </div>
+                    </div>
                 }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
-                    {/* Using 'id' here or removing it from props would stop the linting error */}
-                    <p>{name}</p> 
+                    <p>{name}</p>
                     <img src={assets.rating_starts} alt="" />
                 </div>
                 <p className="food-item-desc">{description}</p>
