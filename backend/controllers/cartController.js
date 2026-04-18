45ae83425c1a76a1353e1b8792c7d5a2e432fa00
Ray <shreyanair2104@gmail.com>
2026-04-19 01:17:55 +0530

    functions for adding, removing, fetching items form cart

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,16 @@
+import userModel from "../models/userModel.js"
+
+//add to cart
+const addToCart = async (req, res) => {
+
+}
+//remove from cart
+const removeFromCart = async (req, res) => {
+
+}
+//get cart
+const getCart = async (req, res) => {
+
+}
+
+export { addToCart, removeFromCart, getCart }
