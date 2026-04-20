7e1513256f1d7aaacae38d3e2601d4d1a648bfb4
Ray <shreyanair2104@gmail.com>
2026-04-21 01:28:11 +0530

    add to csrt functionality

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,7 +2,23 @@ import userModel from "../models/userModel.js"
 
 //add to cart
 const addToCart = async (req, res) => {
+    try{
+      let userData = await userModel.findOne({_id:req.body.userId});
+      let cartData = await userData.cartData;
+      if(!cartData[req.body.itemId])
+      {
+        cartData[req.body.itemId] = 1;
+      }
+      else{
+        cartData[req.body.itemId] += 1; 
+      }
+      await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+      res.json({success:true,message:"Added to cart"})
+    }catch(error){
+      console.log(error);
+      res.json({success:false,message:"Error"});
 
+    }
 }
 //remove from cart
 const removeFromCart = async (req, res) => {
